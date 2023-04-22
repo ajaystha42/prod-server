cf6f683409e9a5ad4588784b01180be80b1cf97d
Ajay Shrestha <er.ajaystha@gmail.com>
2023-04-21 23:47:37 -0400

    cors config

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
       'https://prod-host-ts.vercel.app',
       'http://localhost:3000',
       'http://localhost:3001',
+      'http://localhost:3002',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
