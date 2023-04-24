7f6628672e6a49ea21a1c8a46dfe7feae86ebbfb
Ajay Shrestha <er.ajaystha@gmail.com>
2023-04-24 15:22:06 -0400

    cors config changes

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,18 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    allowedHeaders: '*',
+    origin: [
+      'https://fuse-products.vercel.app',
+      'https://prod-host-ts.vercel.app',
+      'https://prod-cart-ts.vercel.app',
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://localhost:3002',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
-  // app.enableCors({
-  //   allowedHeaders: '*',
-  //   // origin: 'https://fuse-products.vercel.app',
-  //   origin: [
-  //     'https://fuse-products.vercel.app',
-  //     'https://prod-host-ts.vercel.app',
-  //     'https://prod-cart-ts.vercel.app',
-  //     'http://localhost:3000',
-  //     'http://localhost:3001',
-  //     'http://localhost:3002',
-  //   ],
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   credentials: true,
-  // });
   await app.listen(8080);
 }
 bootstrap();
