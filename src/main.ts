12cfb7efdc448200af8a3957f99020691ab4f0aa
Ajay Shrestha <er.ajaystha@gmail.com>
2023-04-24 15:04:57 -0400

    cors configure

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
+    origin: true,
   });
   // app.enableCors({
   //   allowedHeaders: '*',
